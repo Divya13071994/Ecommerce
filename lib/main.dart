bfab6c2de3c884bfad8b5d2736ee45b9d86bf3b8
Divya <divya.borker@pratititech.com>
2022-09-05 17:22:59 +0530

    changes

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_ecart/provider/cart_provider.dart';
+import 'package:flutter_application_ecart/screens/product_list.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+      child: Builder(builder: (context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const ProductList(),
+        );
+      }),
+    );
+  }
+}
